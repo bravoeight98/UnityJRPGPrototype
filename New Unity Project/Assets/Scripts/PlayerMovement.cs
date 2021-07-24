b0eb818626091e828b48bba9e1700d05dd3d8250
Azfar Shams <azfarshams1998@gmail.com>
2021-07-24 17:03:06 +0600

    Update PlayerMovement.cs

--- New Unity Project/Assets/Scripts/PlayerMovement.cs
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
-    
+    private Rigidbody2d rb;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2d>();
     }
 
     // Update is called once per frame
